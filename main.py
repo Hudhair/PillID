05572f1c325a60297d5a080c31f94341392e0edf
HudhairA <148573517+HudhairA@users.noreply.github.com>
2025-12-17 23:17:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+// Function to detect shapes
+def shapeDetection(cap):
+    gray_image = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
+
+    threshold = cv2.threshold(gray_image, 220, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for i, contour in enumerate(contours):
+        if i == 0:
+            continue
+
+        epsilon = 0.01*cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+
+        cv2.drawContours(cap, contour, 0, (0, 0, 255), 5)
